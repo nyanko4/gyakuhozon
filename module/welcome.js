df8efb7d319702b2b493bad9b97d72a8655d71e6
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-04 15:41:41 +0900

    Update welcome.js

--- module/welcome.js
+++ module/welcome.js
@@ -1,12 +1,12 @@
 const supabase = require("../supabase/client");
-const sendchatwork = require("../ctr/message").sendchatwork;
-const cwdata = require("../ctr/cwdata");
-const block = require("../ctr/filter").blockMember;
+const { sendchatwork } = require("../ctr/message");
+const { getChatworkMembers } = require("../ctr/cwdata");
+const { blockMember } = require("../ctr/filter");
 //部屋に参加したら送信する
 async function welcome(body, messageId, roomId) {
   try {
     if (body.match(/\[info\]\[dtext:chatroom_member_is\]\[piconname:\d+\]\[dtext:chatroom_added\]\[\/info\]/g)) {
-      const members = await cwdata.getChatworkMembers(roomId);
+      const members = await getChatworkMembers(roomId);
       const welcomeId = (body.match(/\[piconname:(\d+)\]/) || [])[1];
       const { data } = await supabase
         .from("ブラックリスト")
@@ -19,7 +19,7 @@ async function welcome(body, messageId, roomId) {
         count += person.回数;
       });
       if (reason.includes("荒らし") || count >= 4) {
-        await block(roomId, welcomeId, "ブラックリストに入っています");
+        await blockMember(roomId, welcomeId, "ブラックリストに入っています");
         return
       }
       await sendchatwork(`[rp aid=${welcomeId} to=${roomId}-${messageId}] [pname:${welcomeId}]さん\nよろ〜`,roomId);
