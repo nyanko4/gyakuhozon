057c4c1ff4fadca3d0acb2a7eaa06c6bd2fb1fe5
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-04 16:13:07 +0900

    Update dailytask.js

--- cron/dailytask.js
+++ cron/dailytask.js
@@ -4,7 +4,6 @@ const { sendchatwork } = require("../ctr/message");
 const supabase = require("../supabase/client");
 
 function startDailyTask() {
-    console.log("🟡 startDailyTask() called"); // ← これが Renderログに出るか確認
   new CronJob(
     "0 0 0 * * *",
     async () => {
