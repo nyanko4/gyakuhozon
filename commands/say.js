74c04cacc16f8cf01af29d11bdab8dffb1832611
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-04 14:47:34 +0900

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,14 +1,14 @@
-const isUserAdmin = require("../ctr/cwdata").isUserAdmin;
-const sendchatwork = require("../ctr/message").sendchatwork;
+const { isUserAdmin } = require("../ctr/cwdata");
+const { sendchatwork } = require("../ctr/message");
 //say
 async function displaysay(body, message, messageId, roomId, accountId) {
   try {
     const isAdmin = await isUserAdmin(accountId, roomId);
     const m = body.replace("/say/", "");
     if (!isAdmin) {
-      sendchatwork("管理者のみ利用可能です", roomId)
+      await sendchatwork("管理者のみ利用可能です", roomId)
     } else {
-      sendchatwork(m, roomId);
+      await sendchatwork(m, roomId);
     }
   } catch (error) {
     console.error("errorが発生しました", error);
