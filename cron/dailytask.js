40de7b2067ef938166b9dbfd1e9113d0df035ef9
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-04 14:21:59 +0900

    Update dailytask.js

--- cron/dailytask.js
+++ cron/dailytask.js
@@ -8,7 +8,7 @@ function startDailyTask() {
     "0 0 0 * * *",
     async () => {
       const date = DateTime.now().setZone("Asia/Tokyo").toFormat("yyyy年MM月dd");
-      sendchatwork(`日付変更　今日は${date}日です`, 374987857);
+      await sendchatwork(`日付変更　今日は${date}日です`, 374987857);
       await supabase.from("おみくじ").delete().neq("accountId", 0);
       await supabase.from("poker").delete().neq("accountId", 0);
     },
