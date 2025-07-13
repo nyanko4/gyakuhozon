bfda9f331bee972910841cb3e196da8cf1a5001e
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-13 22:59:07 +0900

    Update readmessage.js

--- commands/readmessage.js
+++ commands/readmessage.js
@@ -3,11 +3,11 @@ const { getChatworkRoom, readmessage2 } = require("../ctr/cwdata");
 
 async function read(body, message, messageId, roomId, accountId) {
   try {
-    let rooms = getChatworkMembers2()
+    const rooms = await getChatworkMembers2()
     for (const room of rooms) {
       const roomid = room.room_id;
         await readmessage2(roomid)
-        );
+    };
   } catch (error) {
     console.error("error:", error);
   }
