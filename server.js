635ca709ede3ebf2226e62ff5e7f7dad6ecb9d7a
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-16 12:10:09 +0900

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(session({
 
 // 認証ミドルウェア
 app.use((req, res, next) => {
-  const publicRoutes = ["/login", "/getchat", "/mention", "/send"];
+  const publicRoutes = ["/login", "/getchat", "/send"];
   if (!publicRoutes.includes(req.path) && req.cookies.nyanko_a !== "ok") {
     req.session.redirectTo = req.path !== "/" ? req.path : null;
     return res.redirect("/login");
