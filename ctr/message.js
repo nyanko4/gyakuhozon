f9e6ad65866911514ed2cc3f7d385273ecd9a836
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-16 23:10:40 +0900

    Update message.js

--- ctr/message.js
+++ ctr/message.js
@@ -1,6 +1,5 @@
 const axios = require("axios");
 const CHATWORK_API_TOKEN = process.env.CWapitoken;
-const CHATWORK_API_TOKEN2 = process.env.CWapitoken2;
 //メッセージを送信
 async function sendchatwork(ms, roomId) {
   try {
@@ -23,26 +22,6 @@ async function sendchatwork(ms, roomId) {
   }
 }
 
-async function sendchatwork2(ms, roomId) {
-  try {
-    await axios.post(
-      `https://api.chatwork.com/v2/rooms/${roomId}/messages`,
-      new URLSearchParams({ body: ms }),
-      {
-        headers: {
-          "X-ChatWorkToken": CHATWORK_API_TOKEN2,
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-    console.log("メッセージ送信成功");
-  } catch (error) {
-    console.error(
-      "Chatworkへのメッセージ送信エラー:",
-      error.response?.data || error.message
-    );
-  }
-}
 //メッセージを削除
 async function deleteMessages(body, messageId, roomId, accountId) {
   const dlmessageIds = [...body.matchAll(/(?<=to=\d+-)(\d+)/g)].map(
@@ -72,24 +51,6 @@ async function deleteMessages(body, messageId, roomId, accountId) {
   }
 }
 
-async function deleteMessage(body, messageId, roomId, accountId) {
-  const url = `https://api.chatwork.com/v2/rooms/${roomId}/messages/${messageId}`;
-
-  try {
-    const response = await axios.delete(url, {
-      headers: {
-        Accept: "application/json",
-        "x-chatworktoken": CHATWORK_API_TOKEN,
-      },
-    });
-  } catch (err) {
-    console.error(
-      `メッセージID ${messageId} の削除中にエラーが発生しました:`,
-      err.response ? err.response.data : err.message
-    );
-  }
-}
-
 //メッセージに既読をつける
 async function readmessage(roomId) {
   try {
@@ -112,32 +73,8 @@ async function readmessage(roomId) {
   }
 }
 
-async function readmessage2(roomId) {
-  try {
-    await axios.put(
-      `https://api.chatwork.com/v2/rooms/${roomId}/messages/read`,
-      new URLSearchParams(),
-      {
-        headers: {
-          'content-type': 'application/json',
-          "X-ChatWorkToken": CHATWORK_API_TOKEN2,
-        },
-      }
-    );
-    console.log("既読をつけました");
-  } catch (error) {
-    console.error(
-      "既読がつけれませんでした:",
-      error.response?.data || error.message
-    );
-  }
-}
-
 module.exports = {
   sendchatwork,
-  sendchatwork2,
   deleteMessages,
-  deleteMessage,
   readmessage,
-  readmessage2,
 };
