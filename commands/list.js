7b86850e6f3630a539f9d0af5ad494cfbadd9e5a
nyanko4 <nyankobetsuaka@outlook.com>
2025-07-18 22:38:41 +0900

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -1,6 +1,14 @@
-async function List(body, messageId, roomId, accountId) {
+const supabase = require("./supabase/client"); // パスは環境によって調整
+async function List(body, message, messageId, roomId, accountId) {
   try {
-    
+    const { data, error } = await supabase
+      .from("名前")
+      .select("name", { count: "exact", head: false })
+      .neq("name", null);
+
+    const uniqueNames = [...new Set(data.map(n => n.name))];
+    await sendchatwork(uniqueNames, roomId); 
+
   } catch(err) {
     console.error(err)
   }
